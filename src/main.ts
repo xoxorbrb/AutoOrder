d82806072cfafa8cb1d5cbb2e32de589b120af2d
xoxorbrb <tg3782@naver.com>
2025-02-03 22:48:23 +0900

    메인 모듈 분리

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import puppeteer, { Browser, Page } from "puppeteer";
+import * as scrapUrls from "./services/urlScraper";
 
 // interface urls {
 //   url: string;
@@ -10,5 +11,26 @@ import puppeteer, { Browser, Page } from "puppeteer";
 //   address: string;
 // }
 
+// 1분마다 새로고침을 위한 1분 변수
+const intervalTime = 60000;
+// 스크랩 루프 상태 관리 플래그
+let isRunning: boolean = true;
+
 const orderSearchWord: string = "굿";
 const contactSearchWord: string = "본부";
+
+async function scrapeAndAutoInput(mainURL: string) {
+  const browser: Browser = await puppeteer.launch();
+  const page: Page = await browser.newPage();
+  await page.goto(mainURL, { waitUntil: "networkidle2" });
+
+  while (isRunning) {
+    const newUrls: string[] = await scrapUrls.scrapeNewUrls(page);
+    newUrls.forEach(async (url: string) => {}); // 스크랩한 url -> 데이터 추출 -> 엑셀 데이터 추가 -> 데이터 자동 입력 -> 클릭까지
+  }
+}
+
+export function stopScrapping() {
+  isRunning = false;
+  console.log("스크랩 중지");
+}
--- src/services/urlScraper.ts
+++ src/services/urlScraper.ts
@@ -1,21 +1,9 @@
 import puppeteer from "puppeteer";
 import type { Page, Browser } from "puppeteer";
 // 이미 스크랩한 url 제외하기 위한 저장할 Set
-const scrapedUrls = new Set<string>();
-// 스크랩 루프 상태 관리 플래그
-let isRunning: boolean = true;
+export const scrapedUrls = new Set<string>();
 
-// 값 가져오는 url 크롤링
-
-export async function scrapeUrls(mainURL: string) {
-  const browser: Browser = await puppeteer.launch();
-  const page: Page = await browser.newPage();
-  await page.goto(mainURL, { waitUntil: "networkidle2" });
-
-  while (isRunning) {} // 여기서 고민좀 해봐야함 1. url 스크랩 -> 데이터 저장 -> 데이터 자동 입력 등등
-}
-
-async function scrapeNewUrls(page: Page) {
+export async function scrapeNewUrls(page: Page) {
   const urls: string[] = await page.evaluate(() => {
     const links = Array.from(document.querySelectorAll("a")); // 링크 수집 (a태그)  // 여기는 수정 필요함
     return links
@@ -31,12 +19,9 @@ async function scrapeNewUrls(page: Page) {
       scrapedUrls.add(url);
       console.log(`URL 저장: ${url}`);
     });
+    return newUrls;
   } else {
     console.log("새로운 url 없음");
+    return [];
   }
-}
-
-export function stopScrapping() {
-  isRunning = false;
-  console.log("스크랩 중지");
 }
