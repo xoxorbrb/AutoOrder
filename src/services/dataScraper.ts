c406aea44c29b45f6684e160e0787d2940821f46
xoxorbrb <tg3782@naver.com>
2025-02-18 18:27:56 +0900

    삼신 리본문구뺀 데이터 크롤링

--- src/services/dataScraper.ts
+++ src/services/dataScraper.ts
@@ -1,18 +1,14 @@
 import puppeteer, { Browser, Page } from "puppeteer";
 
-const keyMapping = {
+const ssKeyMapping = {
   orderNum: "주문번호",
-  status: "상태",
-  orderTime: "발주시간",
-  deliveryTime: "배송시간",
-  clientName: "이마트",
   customerName: "고객명",
-  affiliation: "소속",
   phone: "휴대폰1/휴대폰2",
   telephone: "유선전화",
   deliveryAddress: "배송지",
-  requestComment: "요청사항",
-  hopeDeliveryDate: "희망배송일",
+  flowerName: "화환명",
+  leftText: "좌측 문구",
+  rightText: "우측 문구",
 };
 
 export interface OrderData {
@@ -26,6 +22,8 @@ export interface OrderData {
 }
 export async function ssOrderDataScraper(url: string, browser: Browser) {
   // 삼신 데이터
+  let ssData: { [key: string]: string } = {};
+  ssData.url = url;
   // TODO: URL로부터 데이터 수집
   const page: Page = await browser.newPage();
 
@@ -33,5 +31,29 @@ export async function ssOrderDataScraper(url: string, browser: Browser) {
 
   // const rowHeightKeyClass: string[]= ["table-blue", "table-green", "table-gray"]; // 키를 가지고 있는 태그의 클래스 속성
 
-  const data = await page.$$eval(".row-height", (row) => {});
+  //데이터 추출
+  await page.$$eval(".row-height", (rows) => {
+    rows.forEach((row) => {
+      const keyTags: HTMLElement[] = Array.from(
+        row.querySelectorAll(".table-blue, .table-green")
+      );
+
+      const valueTag = Array.from(row.querySelectorAll(".inside")).filter(
+        (el) => !el.closest(".table-blue, .table-green")
+      );
+
+      keyTags.forEach((keyTag, index) => {
+        const keyInsideElement = keyTag.querySelector(".inside");
+        const valueInsideElement = valueTag[index];
+        const key: string = keyInsideElement?.textContent?.trim() || "";
+        const value: string = valueInsideElement?.textContent?.trim() || "";
+
+        if (Object.values(ssKeyMapping).includes(key)) {
+          ssData[key] = value;
+        }
+      });
+    });
+  });
+
+  await page.$$eval(".row-height, .line-t", (row) => {});
 }
