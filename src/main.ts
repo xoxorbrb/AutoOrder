fea9b7a31157b3dc184e45fdc00ae675d52d7b9a
xoxorbrb <tg3782@naver.com>
2025-02-24 22:25:41 +0900

    로그인 모듈화  + RNM 자동로그인 개발

--- src/main.ts
+++ src/main.ts
@@ -16,8 +16,7 @@ let isRunning: boolean = true;
 
 const orderSearchWord: string = "굿";
 const contactSearchWord: string = "본부";
-const ssUrl: string = "https://samsincall.com/partners/orders/";
-const roseUrl: string = "http://16441644.roseweb.co.kr/index.htm";
+
 async function scrapeAndAutoInput() {
   const ssBrowser: Browser = await puppeteer.launch(); //samsin 브라우저
   const ssPage: Page = await ssBrowser.newPage();
@@ -35,105 +34,4 @@ async function scrapeAndAutoInput() {
 export function stopScrapping() {
   isRunning = false;
   console.log("스크랩 중지");
-}
-
-// 로그인 상태 확인 및 로그인 자동화
-async function sessionCheckAndSetLogin(
-  page: Page,
-  url: string,
-  id: string,
-  password: string,
-  key: string = "" // 플라워 인트라넷 key 값
-) {
-  await page.goto(url, {
-    waitUntil: "load",
-  });
-
-  if (url === ssUrl) {
-    // 알림 감지
-    let alertOn = false;
-    page.on("dialog", async (dialog) => {
-      alertOn = true;
-      await dialog.dismiss(); // 알럿창 끄기
-    });
-    if ((alertOn = true)) {
-      await page.reload({ waitUntil: "load" });
-
-      const currentUrl = page.url();
-
-      if (currentUrl.includes("/login")) {
-        console.log("[삼신상사] 로그인되지 않은 상태, 로그인 필요");
-        await page.evaluate(() => {
-          const idField = document.querySelector(
-            'input[name="member_id"]'
-          ) as HTMLInputElement;
-          const pwField = document.querySelector(
-            'input[name="member_pw"]'
-          ) as HTMLInputElement;
-          idField.value = ""; //아이디 입력 필드 초기화 (중복방지)
-          pwField.value = ""; //패스워드 입력 필드 초기화  (중복방지)
-        });
-
-        // 로그인 정보 입력
-        await page.type('input[name="member_id"]', id);
-        await page.type('input[name="member_pw"]', password);
-
-        await Promise.all([
-          page.click('input[id="btn-login"]'),
-          page.waitForNavigation({ waitUntil: "load" }),
-        ]);
-
-        let afterUrl = page.url();
-
-        if (!afterUrl.includes("/login")) {
-          console.log("[삼신상사] 로그인 성공");
-        } else {
-          console.log("[삼신상사] 로그인 실패");
-          stopScrapping();
-        }
-      } else {
-        console.log("[삼신상사]로그인 되어있음");
-      }
-    }
-  } else if (url === roseUrl) {
-    let loginButtonExists = (await page.$('input[alt="로그인버튼"]')) !== null;
-
-    if (loginButtonExists) {
-      console.log("[플라워 인트라넷] 로그인 되지 않은 상태");
-
-      await page.evaluate(() => {
-        const idField = document.querySelector(
-          'input[name="sid"]'
-        ) as HTMLInputElement;
-        const pwField = document.querySelector(
-          'input[name="spw"]'
-        ) as HTMLInputElement;
-        const keyField = document.querySelector(
-          'input[name="skey"]'
-        ) as HTMLInputElement;
-
-        //입력필드 초기화
-        idField.value = "";
-        pwField.value = "";
-        keyField.value = "";
-      });
-      // 로그인 정보 입력
-      await page.type('input[name="sid"]', id);
-      await page.type('input[name="spw"]', password);
-      await page.type('input[name="skey"]', key);
-
-      await Promise.all([
-        page.click('input[alt="로그인버튼"]'),
-        page.waitForNavigation({ waitUntil: "load" }),
-      ]);
-      loginButtonExists = (await page.$('input[alt="로그인버튼"]')) !== null;
-
-      if (loginButtonExists) {
-        console.log("[플라워 인트라넷] 로그인 성공");
-      } else {
-        console.log("[플라워 인트라넷] 로그인 실패");
-        stopScrapping();
-      }
-    }
-  }
 }
---
+++ src/services/autoLogin.ts
@@ -0,0 +1,144 @@
+import type { Page } from "puppeteer";
+
+const ssUrl: string = "https://samsincall.com/partners/orders/"; //삼신상사
+const roseUrl: string = "http://16441644.roseweb.co.kr/index.htm"; // 플라워 인트라넷
+const rnmUrl: string = "http://16005423.co.kr/agent/"; // rnm 발주페이지
+// 로그인 상태 확인 및 로그인 자동화
+export async function sessionCheckAndSetLogin(
+  page: Page,
+  url: string,
+  id: string,
+  password: string,
+  key: string = "" // 플라워 인트라넷 key 값
+): Promise<Boolean> {
+  await page.goto(url, {
+    waitUntil: "load",
+  });
+
+  if (url === ssUrl) {
+    // 알림 감지
+    let alertOn: Boolean = false;
+    page.on("dialog", async (dialog) => {
+      alertOn = true;
+      await dialog.dismiss(); // 알럿창 끄기
+    });
+    if ((alertOn = true)) {
+      await page.reload({ waitUntil: "load" });
+
+      const currentUrl = page.url();
+
+      if (currentUrl.includes("/login")) {
+        console.log("[삼신상사] 로그인되지 않은 상태, 로그인 필요");
+        await page.evaluate(() => {
+          const idField = document.querySelector(
+            'input[name="member_id"]'
+          ) as HTMLInputElement;
+          const pwField = document.querySelector(
+            'input[name="member_pw"]'
+          ) as HTMLInputElement;
+          idField.value = ""; //아이디 입력 필드 초기화 (중복방지)
+          pwField.value = ""; //패스워드 입력 필드 초기화  (중복방지)
+        });
+
+        // 로그인 정보 입력
+        await page.type('input[name="member_id"]', id);
+        await page.type('input[name="member_pw"]', password);
+
+        await Promise.all([
+          page.click('input[id="btn-login"]'),
+          page.waitForNavigation({ waitUntil: "load" }),
+        ]);
+
+        let afterUrl = page.url();
+
+        if (!afterUrl.includes("/login")) {
+          console.log("[삼신상사] 로그인 성공");
+          return true;
+        } else {
+          console.log("[삼신상사] 로그인 실패");
+          return false;
+        }
+      } else {
+        console.log("[삼신상사]로그인 되어있음");
+        return true;
+      }
+    }
+  } else if (url === roseUrl) {
+    let loginButtonExists = (await page.$('input[alt="로그인버튼"]')) !== null;
+
+    if (loginButtonExists) {
+      console.log("[플라워 인트라넷] 로그인 되지 않은 상태");
+
+      await page.evaluate(() => {
+        const idField = document.querySelector(
+          'input[name="sid"]'
+        ) as HTMLInputElement;
+        const pwField = document.querySelector(
+          'input[name="spw"]'
+        ) as HTMLInputElement;
+        const keyField = document.querySelector(
+          'input[name="skey"]'
+        ) as HTMLInputElement;
+
+        //입력필드 초기화
+        idField.value = "";
+        pwField.value = "";
+        keyField.value = "";
+      });
+      // 로그인 정보 입력
+      await page.type('input[name="sid"]', id);
+      await page.type('input[name="spw"]', password);
+      await page.type('input[name="skey"]', key);
+
+      await Promise.all([
+        page.click('input[alt="로그인버튼"]'),
+        page.waitForNavigation({ waitUntil: "load" }),
+      ]);
+      loginButtonExists = (await page.$('input[alt="로그인버튼"]')) === null;
+
+      if (loginButtonExists) {
+        console.log("[플라워 인트라넷] 로그인 성공");
+        return true;
+      } else {
+        console.log("[플라워 인트라넷] 로그인 실패");
+        return false;
+      }
+    }
+  } else if (url === rnmUrl) {
+    let loginButtonExists =
+      (await page.$("button.btn.btn-primary.btn-block.btn-flat")) !== null;
+
+    if (loginButtonExists) {
+      console.log("[RNM] 로그인 되어있지 않음");
+
+      await page.evaluate(() => {
+        const idField = document.querySelector(
+          "input[name=in_uid]"
+        ) as HTMLInputElement;
+        const pwField = document.querySelector(
+          "input[name=in_upw]"
+        ) as HTMLInputElement;
+
+        idField.value = "";
+        pwField.value = "";
+      });
+      await page.type('input[name="in_uid"]', id);
+      await page.type('input[name="in_upw"]', password);
+
+      await Promise.all([
+        page.click("button.btn.btn-primary.btn-block.btn-flat"),
+        page.waitForNavigation({ waitUntil: "load" }),
+      ]);
+      loginButtonExists = (await page.$('input[alt="로그인버튼"]')) === null;
+
+      if (loginButtonExists) {
+        console.log("[RNM] 로그인 성공");
+        return true;
+      } else {
+        console.log("[RNM] 로그인 실패");
+        return false;
+      }
+    }
+  }
+  return true;
+}
