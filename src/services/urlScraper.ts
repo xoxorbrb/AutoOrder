3fca56973f4fa33d08987763d5b3d43be92ff3d3
xoxorbrb <tg3782@naver.com>
2025-03-11 00:07:26 +0900

    url 스크랩 개발(삼신)

--- src/services/urlScraper.ts
+++ src/services/urlScraper.ts
@@ -6,30 +6,56 @@ export async function ssScrapeNewUrls(
   page: Page,
   date: string
 ): Promise<string[]> {
-  const inputDate = parseCustomDateTime(date);
-  //삼신상사
-  const dates: string[] = await page.$$eval(
-    "div[style=color: rgb(254, 71, 71)]",
-    (divs) => {
-      return divs.map((div) => div.textContent || "");
-    }
-  );
+  // //삼신상사
+  // const dates: string[] = await page.$$eval(
+  //   "div[style=color: rgb(254, 71, 71)]",
+  //   (divs) => {
+  //     return divs.map((div) => div.textContent || "");
+  //   }
+  // );
 
-  const urls: string[] =
-    (await page.$$eval(
-      "a",
-      (anchors, baseUrl) => {
-        return anchors
-          .map((anchor) => {
-            const href = (anchor as HTMLAnchorElement).getAttribute("href");
-            return new URL(href || "", baseUrl).href as string;
-          })
-          .filter((url: string) => {
-            url.includes("/detail/?order_no=");
-          });
-      },
-      page.url() //baseUrl 매개변수 값
-    )) || [];
+  // const urls: string[] =
+  //   (await page.$$eval(
+  //     "a",
+  //     (anchors, baseUrl) => {
+  //       return anchors
+  //         .map((anchor) => {
+  //           const href = (anchor as HTMLAnchorElement).getAttribute("href");
+  //           return new URL(href || "", baseUrl).href as string;
+  //         })
+  //         .filter((url: string) => {
+  //           url.includes("/detail/?order_no=");
+  //         });
+  //     },
+  //     page.url() //baseUrl 매개변수 값
+  //   )) || [];
+
+  const inputDate = parseCustomDateTime(date);
+  let urls: string[] = [];
+
+  await page.$$eval(
+    "tr",
+    (rows, baseUrl) => {
+      rows.forEach((row) => {
+        const computedStyle = window.getComputedStyle(row);
+        if (computedStyle.backgroundColor !== "rgb(227, 243, 246)") return;
+
+        const tds = row.querySelectorAll("td");
+        const linkElement = tds[0]?.querySelector("a"); // 첫 번째 td에서 a 태그 찾기 (url)
+        const dateText =
+          tds[1]?.querySelector("div")?.textContent?.trim() || ""; // 두 번째 td의 첫 div 찾기 (시간)
+
+        if (linkElement && dateText) {
+          const rowDate = parseCustomDateTime(dateText);
+
+          if (rowDate > inputDate) {
+            urls.push(new URL(linkElement.href, baseUrl).href); // URL 절대경로로 변환
+          }
+        }
+      });
+    },
+    page.url()
+  );
 
   const newUrls = urls.filter((url) => !ssScrapedUrls.has(url)) as string[]; // 이전에 데이터를 얻어온 url을 제외한 url가져오기
 
@@ -46,27 +72,32 @@ export async function ssScrapeNewUrls(
   }
 }
 
-export async function roseScrapedNewUrls(page: Page): Promise<string[]> {
+export async function roseScrapedNewUrls(
+  page: Page,
+  date: string
+): Promise<string[]> {
   let urls: string[] = [];
-  await page.$$eval(
-    "a",
-    (anchors, baseUrl) => {
-      anchors.forEach((anchor) => {
-        const onClickAttr = (anchor as HTMLAnchorElement).getAttribute(
-          "onCiick"
-        );
-        if (onClickAttr && onClickAttr.length > 0) {
-          const urlMatch: RegExpMatchArray | null = onClickAttr.match(
-            /window\.open\(['"]([^'"]+)['"]/
-          );
-          const url = new URL(urlMatch ? urlMatch[1] : "", baseUrl)
-            .href as string;
-          urls.push(url);
-        }
-      });
-    },
-    page.url()
-  );
+  const inputDate = parseCustomDateTime(date);
+
+  // await page.$$eval(
+  //   "a",
+  //   (anchors, baseUrl) => {
+  //     anchors.forEach((anchor) => {
+  //       const onClickAttr = (anchor as HTMLAnchorElement).getAttribute(
+  //         "onCiick"
+  //       );
+  //       if (onClickAttr && onClickAttr.length > 0) {
+  //         const urlMatch: RegExpMatchArray | null = onClickAttr.match(
+  //           /window\.open\(['"]([^'"]+)['"]/
+  //         );
+  //         const url = new URL(urlMatch ? urlMatch[1] : "", baseUrl)
+  //           .href as string;
+  //         urls.push(url);
+  //       }
+  //     });
+  //   },
+  //   page.url()
+  // );
 
   const newUrls = urls.filter((url) => !roseScrapedUrls.has(url)) as string[];
 
